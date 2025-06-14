9f10664d1671729b701f63442570383be45fc05f
manal-elmarfoussi10 <manalelmarfoussi@gmail.com>
2025-06-14 02:59:32 +0100

    correct registration

--- database/migrations/2025_06_14_003925_add_role_to_users_table.php
+++ database/migrations/2025_06_14_003925_add_role_to_users_table.php
@@ -9,20 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            //
+            $table->string('role')->default('client');
         });
     }
 
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            //
-        });
-    }
+    public function down()
+{
+    Schema::table('users', function (Blueprint $table) {
+        $table->dropColumn('role');
+    });
+}
 };
