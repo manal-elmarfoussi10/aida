f5765a019d8553314d13bd512583cdbd1c7fec04
manal-elmarfoussi10 <manalelmarfoussi@gmail.com>
2025-06-14 03:10:45 +0100

    hello test

--- resources/views/admin/admin.blade.php
+++ resources/views/admin/admin.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="max-w-4xl mx-auto bg-gray-800 p-8 rounded-lg shadow-lg">
     <h1 class="text-3xl font-bold mb-4 text-green-400">Admin Dashboard</h1>
-    <p>Welcome, {{ Auth::user()->name }}!</p>
+    <p>Hello, {{ Auth::user()->name }}!</p>
 
     {{-- Add your admin widgets here --}}
     <div class="mt-8">
