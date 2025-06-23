8baa687919bf0dbdc01af7792d04500756a3ea1a
elaabous <elaabousfati.2019@gmail.com>
2025-06-23 19:43:09 -0100

    dashboard

--- resources/views/client/dashboard.blade.php
+++ resources/views/client/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 @section('content')
-    <h1>Welcome to your Dashboard, {{ auth()->user()->name }}</h1>
+    <h1>Welcome to  Dashboard, {{ auth()->user()->name }}</h1>
     <!-- Add widgets and info for clients here -->
 @endsection
